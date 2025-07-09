0eb48f1422b006f8124045ea15a3f28e39632b30
Swathidattha02 <swathipasupuleti02@gmail.com>
2025-07-09 23:09:19 +0530

    reverse a number

--- Dsa.java
+++ Dsa.java
@@ -18,24 +18,53 @@
 
 
 
+// import java.util.*;
+// class Main {
+//     public static boolean duplicates(int arr[])
+//     {
+//          HashSet <Integer> set=new HashSet<>();
+//          for(int x:arr)
+//          {
+//              if(set.contains(x))
+//              {
+//                  return true;
+//              }
+//              set.add(x);
+//          }
+//          return false;
+//     }
+//     public static void main(String[] args) 
+//     {
+//         int  arr[]={1,2,3,1,3};
+//         System.out.println(duplicates(arr));
+//     }
+// }
+
+
 import java.util.*;
-class Main {
-    public static boolean duplicates(int arr[])
+class Dsa
+{
+    public static void main(String args[])
     {
-         HashSet <Integer> set=new HashSet<>();
-         for(int x:arr)
-         {
-             if(set.contains(x))
-             {
-                 return true;
-             }
-             set.add(x);
-         }
-         return false;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("the number is :"+ n);
+        // int rev=0;
+        // while(n!=0)
+        // {
+        //     rev=rev*10 + n%10;
+        //     n=n/10;
+            
+        // }
+        // StringBuffer st=new StringBuffer(String.valueOf(n));
+        // StringBuffer rev=st.reverse();
+        // System.out.println("the reversed number is :"+ rev);
+
+        StringBuilder sb=new StringBuilder();
+        sb.append(n);
+        StringBuilder rev=sb.reverse();
+        System.out.println("the reversed number is :"+ rev);
+
     }
-    public static void main(String[] args) 
-    {
-        int  arr[]={1,2,3,1,3};
-        System.out.println(duplicates(arr));
-    }
+
 }
