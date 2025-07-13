9e55af2f97044b66ca50b50cbe1c0b6cf2797d5d
Swathidattha02 <swathipasupuleti02@gmail.com>
2025-07-13 11:32:57 +0530

    2darray

---
+++ ZuvyProblems.java
@@ -0,0 +1,31 @@
+// class ZuvyProblems {
+//     public static boolean isPalindrome(int arr[])
+//     {
+//         for(int i=0;i<arr.length/2;i++)
+//         {
+//             if(arr[i]!=arr[arr.length-i-1])
+//             {
+//                 return  false;
+//             }
+//         }
+//         return true;
+//     }
+//     public static void main(String[] args) {
+//         int arr[]={1,2,3,2,1};
+//         System.out.println("is the array palindrome : "+isPalindrome(arr));
+//     }
+// }
+class ZuvyProblems {
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=arr[i].length-1;j>=0;j--)
+            {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+        
+    }
+}
